346cd49e37f91da1334bdf03a80fe34fa541fe11
zurimokato <noj2304@gmail.com>
2018-03-14 21:43:14 -0500

    Agregando funcionalidad de multiplicacion y division

--- funcionalidad.js
+++ funcionalidad.js
@@ -3,11 +3,27 @@ function  Calculadora(numA, numB){
 	this.numB=numB;
 	this.sumar=function(){
 		var suma=this.numA+this.numB;
-		alert(suma)
+		alert(suma);
 		return suma;	
 	}
 	this.restar=function(){
-		
+		var restar=this.numA-this.numB;
+		alert(restar);
+		return restar;
+	}
+	this.multiplicar=function(){
+		var producto=this.numA*this.numB;
+		alert(producto);
+		return producto;
+	}
+	this.dividir=function(){
+		if(this.numB!=0){
+		var cociente=this.numA/this.numB;
+		alert(cociente);
+		}else{
+			alert("no se puede dividir por cero");
+		}
+		return cociente;
 	}
 }
 
@@ -15,6 +31,23 @@ function  Calculadora(numA, numB){
 function operar(){
 	var numA=parseInt(document.getElementById("numA").value);
 	var numB=parseInt(document.getElementById("numB").value);
+	var operando = document.getElementById("mySelect").value;
 	var cal=new Calculadora(numA,numB);
-	var suma=cal.sumar();
+	if (operando=='sumar') {
+		var suma=cal.sumar();
+	}else{
+		if (operando=='restar') {
+			var restar=cal.restar();
+		}else{
+			if (operando=='multiplicar') {
+				var restar=cal.multiplicar();
+			}else{
+				if (operando=='dividir') {
+					var restar=cal.dividir();
+				}
+			}
+		}
+	}
+	
+	
 }
